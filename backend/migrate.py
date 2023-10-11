2198c09d7adcb9886655f91ab87393bedeaeca87
aidan <a.kelly.2018@uni.strath.ac.uk>
2023-10-11 22:21:15 +0100

    implement tables

--- backend/migrate.py
+++ backend/migrate.py
@@ -10,12 +10,92 @@ migrate = Migrate(app, db)
 
 class Referral(db.Model):
     __tablename__ = 'referrals'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
 
-    def __init__(self, name):
-        self.name = name
+    GPName = db.Column(db.String())
+    GPAddress = db.Column(db.String())
+    GPPhone = db.Column(db.String())
+
+    # fields used to calculate priority score
+    ReceivedTherapyBefore = db.Column(db.Boolean())
+    usedPsychologicalServices = db.Column(db.Boolean())
+    MentalHealthDiagnosis = db.Column(db.Boolean())
+    MentalHealthAssessment = db.Column(db.Boolean())
+    SelfHarm = db.column(db.Boolean())
+    Medication = db.Column(db.Boolean())
+    TraumaticExperience = db.Column(db.Boolean())
+    ProfessionalsWorkingWithYou = db.Column(db.Boolean())
+    MainDifficulty = db.Column(db.String()) #used for sentiment analysis
+    PriorityScore = db.Column(db.Integer())
+
+    Notes = db.Column(db.String())
+    ReferralType = db.Column(db.String())
+
+    # virtual or in person
+    preferredTherapyMethod = db.Column(db.String())
+
+    # preferred Time
+    preferredTime = db.Column(db.String())
+
+    eligibleForSupport = db.Column(db.Boolean())
+    isProcessed = db.Column(db.Boolean())
+
+    TherapistId = db.Column(db.Integer, db.ForeignKey('therapists.id'), nullable=True)
+
+    def __init__(self, FirstName, Surname, DateOfBirth, Address, GPName, GPAddress, 
+                 GPPhone, ReceivedTherapyBefore, usedPsychologicalServices, 
+                 MentalHealthDiagnosis, MentalHealthAssessment, SelfHarm, 
+                 Medication, TraumaticExperience, ProfessionalsWorkingWithYou, 
+                 MainDifficulty, PriorityScore, Notes, ReferralType,
+                 preferredTherapyMethod, preferredTime, eligibleForSupport, isProcessed
+                 ):
+        self.FirstName = FirstName
+        self.Surname = Surname
+        self.DateOfBirth = DateOfBirth
+        self.Address = Address
+        self.GPName = GPName
+        self.GPAddress = GPAddress
+        self.GPPhone = GPPhone
+        self.ReceivedTherapyBefore = ReceivedTherapyBefore
+        self.usedPsychologicalServices = usedPsychologicalServices
+        self.MentalHealthDiagnosis = MentalHealthDiagnosis
+        self.MentalHealthAssessment = MentalHealthAssessment
+        self.SelfHarm = SelfHarm
+        self.Medication = Medication
+        self.TraumaticExperience = TraumaticExperience
+        self.ProfessionalsWorkingWithYou = ProfessionalsWorkingWithYou
+        self.MainDifficulty = MainDifficulty
+        self.PriorityScore = PriorityScore
+        self.Notes = Notes
+        self.ReferralType = ReferralType
+        self.preferredTherapyMethod = preferredTherapyMethod
+        self.preferredTime = preferredTime
+        self.eligibleForSupport = eligibleForSupport
+        self.isProcessed = isProcessed  
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+    
+    
+class Therapist(db.Model):
+    __tablename__ = 'therapists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String())
+    Surname = db.Column(db.String())
+    DateOfBirth = db.Column(db.Date())
+    OfficeAddress = db.Column(db.String())
+    TherapistType = db.Column(db.String())
+    ClientIds = db.relationship('Referral', backref='therapists', lazy=True)
+    
+    def __init__(self, FirstName, Surname, DateOfBirth, OfficeAddress, TherapistType):
+        self.FirstName = FirstName
+        self.Surname = Surname
+        self.DateOfBirth = DateOfBirth
+        self.OfficeAddress = OfficeAddress
+        self.TherapistType = TherapistType
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
