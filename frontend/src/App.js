9560c1bce9fe151b455888cdea96ecbb3a61e547
Minh Nguyet Le <lenguyetminhh0210@gmail.com>
2023-10-12 11:05:18 +0100

    update tabs when referrals processed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,81 @@
 import './App.css';
+import React, { useState } from 'react';
 import Table from './components/table/Table';
-
-function App() {
 
   //fetch list of referals
   const referrals = [
-    { name: 'John Doe', location: 'New York', age: 25, difficulty: 'Easy', isFlagged: false, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
-    { name: 'Jane Doe', location: 'San Francisco', age: 30, difficulty: 'Medium', isFlagged: false, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
-    { name: 'Jim Doe', location: 'Los Angeles', age: 35, difficulty: 'Hard', isFlagged: true, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
+    { id:0, name: 'John Doe', location: 'New York', age: 25, difficulty: 'Easy', isFlagged: false, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
+    { id:1, name: 'Jane Doe', location: 'San Francisco', age: 30, difficulty: 'Medium', isFlagged: false, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
+    { id:2, name: 'Jim Doe', location: 'Los Angeles', age: 35, difficulty: 'Hard', isFlagged: true, date: '20/02/9999', isProcessed: false, eligibleForSupport: false, virtual: false, inPerson: false, timeAM: false, timePM: false},
   ];
+
+  
+function App() {
+  const [data, setData] = useState(referrals);
+
+  function handleUpdateEligible(index, Virtual, inPerson) {
+    var newReferrals = [...referrals];
+    newReferrals[index].eligibleForSupport = true;
+    newReferrals[index].isProcessed = true;
+    console.log("Check here", Virtual);
+    newReferrals[index].virtual = Virtual;
+    newReferrals[index].inPerson = inPerson;
+    setData(newReferrals);
+  }
+
+  function handleUpdateNotEligible(index) {
+    var newReferrals = [...referrals];
+    newReferrals[index].eligibleForSupport = false;
+    newReferrals[index].isProcessed = true;
+    setData(newReferrals);
+  }
+
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
 
   return (
     <div>
-      <h1>Referrals</h1>
-      <Table data={referrals} />
+      <div className="tab">
+        <button
+          className={activeTab === 1 ? 'active' : ''}
+          onClick={() => handleTabClick(1)}
+        >
+          Tab 1
+        </button>
+        <button
+          className={activeTab === 2 ? 'active' : ''}
+          onClick={() => handleTabClick(2)}
+        >
+          Tab 2
+        </button>
+        <button
+          className={activeTab === 3 ? 'active' : ''}
+          onClick={() => handleTabClick(3)}
+        >
+          Tab 3
+        </button>
+      </div>
+
+      <div className="tab-content">
+        {activeTab === 1 && 
+        (<div> 
+          <h1>Pending Referrals</h1>
+            <Table typeOfTable='referral' data={data} updateEligible={(index, Virtual, inPerson) => handleUpdateEligible(index, Virtual, inPerson)} updateNotEligible={(index) => handleUpdateNotEligible(index)} />
+          </div>
+        )}
+        {activeTab === 2 && 
+        (<div> 
+          <h1>Therapist Waiting List</h1>
+            <Table typeOfTable='waitlist' data={data} updateEligible={(index) => handleUpdateEligible(index)} updateNotEligible={(index) => handleUpdateNotEligible(index)} />
+          </div>
+        )}
+        {/* {activeTab === 3 && <div>Content for Tab 3</div>} */}
+      </div>
     </div>
+
   );
 }
 
--- frontend/src/components/table/Table.js
+++ frontend/src/components/table/Table.js
@@ -1,15 +1,16 @@
 import Referal from "../referal/Referal";
+import Waitlist from "../waitlist/Waitlist";
 
-function Table({ data }) {
+function Table({typeOfTable, data, updateEligible, updateNotEligible }) {
     return (
+      <>
+      {typeOfTable == 'referral' &&
       <div>
         {data.map((referral, index) => 
-        
-          
-
             (
             <Referal
             key={index}
+            id={referral.id}
             name={referral.name}
             location={referral.location}
             age={referral.age}
@@ -22,13 +23,39 @@ function Table({ data }) {
             inPerson={referral.inPerson}
             timeAM={referral.timeAM}
             timePM={referral.timePM}
+            updateEligible={updateEligible}
+            updateNotEligible={updateNotEligible}
           />
-          
-
-          
         ))}
-      
       </div>
+      }
+      {typeOfTable == 'waitlist' &&
+      <div>
+        {data.map((referral, index) => 
+            (
+            <Waitlist
+            key={index}
+            id={referral.id}
+            name={referral.name}
+            location={referral.location}
+            age={referral.age}
+            difficulty={referral.difficulty}
+            isFlagged={referral.isFlagged}
+            date={referral.date}
+            eligibleForSupport={referral.eligibleForSupport}
+            isProcessed={referral.isProcessed}
+            virtual={referral.virtual}
+            inPerson={referral.inPerson}
+            timeAM={referral.timeAM}
+            timePM={referral.timePM}
+            updateEligible={updateEligible}
+            updateNotEligible={updateNotEligible}
+          />
+        ))}
+      </div>
+      }
+
+      </>
     );
   }
 
